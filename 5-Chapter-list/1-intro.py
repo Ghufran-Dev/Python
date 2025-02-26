e427b87f59fb180a6a80c8ea9780553dc5ae7d7a
Muhammad Ghufran <darazone04@gmail.com>
2025-02-26 16:21:55 +0500

    intro

---
+++ 5-Chapter-list/1-intro.py
@@ -0,0 +1,12 @@
+# data structure
+# order collection of items
+# you can store anything in list hetrogenous string float list 
+# array are homogenous same things 
+number = [1,2,3,4.0,5.2]
+words =['a', 'b', 'c', ['fruits', 'apple', 'mango']]
+
+mixed = [*number, *words]
+print(mixed)
+print(number[::2])
+print(number[-1])
+print(number[1:])
